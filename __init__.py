2908b27e17c4ce384ea32ab076f23935ab151a9b
lopezfr <lopezfr@sec.gov>
2025-02-12 15:13:46 -0500

    adding readme and documenting Arelle version

---
+++ __init__.py
@@ -0,0 +1,10 @@
+__pluginInfo__ = {
+    'name': 'EDGAR',
+    'version': '1.25.1',
+    'description': "This plug-in implements U.S. SEC Edgar Renderer. Arelle version at SEC: 2.36.11 ",
+    'license': 'Apache-2',
+    'author': 'U.S. SEC Employees and Mark V Systems Limited',
+    'copyright': '(c) Portions by SEC Employees not subject to domestic copyright, otherwise (c) Copyright 2015 Mark V Systems Limited, All rights reserved.',
+    'aliases': ('EdgarRenderer',),
+    'import': ('EDGAR/render', )
+}
---
+++ readme.md
@@ -0,0 +1,7 @@
+# EDGAR Arelle Plugin
+
+## Overview
+The EDGAR plugin, developed and maintained by the staff of the U.S. Securities and Exchange Commission (SEC), is designed to provide traditional and inline XBRL viewers for SEC filings. It also integrates with and extends the EFM Validation plugin, offering EFM validation for SEC filings. For end-user support, please contact the SEC directly at: StructuredData@sec.gov.
+
+## Arelle Version
+The current version of Arelle in use at the SEC is: **2.36.11**
