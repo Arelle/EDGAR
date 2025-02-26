43751d9f3fa7d98602cfe71516b4c33100ffd1ad
lopezfr <lopezfr@sec.gov>
2025-02-26 17:00:02 -0500

    updating Arelle version number

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 __pluginInfo__ = {
     'name': 'EDGAR',
     'version': '3.25.1',
-    'description': "This plug-in implements U.S. SEC Edgar Renderer. Arelle version at SEC: 2.36.14 ",
+    'description': "This plug-in implements U.S. SEC Edgar Renderer. Arelle version at SEC: 2.36.16 ",
     'license': 'Apache-2',
     'author': 'U.S. SEC Employees and Mark V Systems Limited',
     'copyright': '(c) Portions by SEC Employees not subject to domestic copyright, otherwise (c) Copyright 2015 Mark V Systems Limited, All rights reserved.',
